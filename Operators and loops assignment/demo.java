e809021a8e317fe08e8f48f02ad65c76d66808f3
hacker-ram <hackerram09@gmail.com>
2023-11-09 22:59:49 +0530

    Operators and loops

---
+++ Operators and loops assignment/demo.java
@@ -0,0 +1,12 @@
+//code to print numbers from 1 to 10
+public class demo {
+    
+    public static void main(String[] args) {
+        for(int i=1 ;i<=10;i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
+
+    
+}
